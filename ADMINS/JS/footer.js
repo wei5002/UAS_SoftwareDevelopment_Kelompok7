94213d2a4a60c63edbad7ec5ae151f0227cdd020
claresta1704 <duoce2005@gmail.com>
2025-05-15 13:59:53 +0700

    navbar header footer, home new orders sales report

---
+++ ADMINS/JS/footer.js
@@ -0,0 +1,6 @@
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer-placeholder").innerHTML = data;
+  })
+  .catch((error) => console.error("Error loading footer:", error));
---
+++ ADMINS/JS/header-logout.js
@@ -0,0 +1,6 @@
+fetch("header-logout.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header-placeholder").innerHTML = data;
+  })
+  .catch((error) => console.error("Error loading footer:", error));
